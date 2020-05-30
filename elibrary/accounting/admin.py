0006bad3e73ad5c48c7e50a2ece575701c73edbc
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-05-30 17:11:37 +0300

    Register models in the admin.

--- elibrary/accounting/admin.py
+++ elibrary/accounting/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['author_name', 'author_surname']
+    ordering = ['author_name']
+
+
+@admin.register(ScienceBook)
+class ScienceBookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publisher', 'edition', 'publishing_year', 'isbn']
+    ordering = ['title']
+    filter_horizontal = ['work_author']    # two-sided areas for multiple choice (for ManyToMany)
+
+
+@admin.register(FictionBook)
+class FictionBookAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    ordering = ['title']
+    filter_horizontal = ['work_author']
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'journal', 'impact_factor',
+                    'volume', 'article_number', 'pages', 'publishing_year', 'doi']
+    ordering = ['title']
+    filter_horizontal = ['work_author']
+
+
+@admin.register(LibraryUserInfo)
+class LibraryUserInfoAdmin(admin.ModelAdmin):
+    list_display = ['library_user', 'phone_number']
+    ordering = ['library_user']
+
+
+@admin.register(CitiesList)
+class CitiesListAdmin(admin.ModelAdmin):
+    list_display = ['city_name']
+
+
+@admin.register(StreetsList)
+class StreetsListAdmin(admin.ModelAdmin):
+    list_display = ['street_name']
+
+
+@admin.register(LibraryUserAddress)
+class LibraryUserAddressAdmin(admin.ModelAdmin):
+    list_display = ['building_number', 'apartment_number']
--- elibrary/accounting/models.py
+++ elibrary/accounting/models.py
@@ -7,11 +7,19 @@ class Author(models.Model):
     author_name = models.CharField(max_length=100)
     author_surname = models.CharField(max_length=100)
 
+    def __str__(self):
+        name_surname = [self.author_name, self.author_surname]
+        name_surname = " ".join(name_surname)
+        return name_surname
+
 
 class FictionBook(models.Model):
     work_author = models.ManyToManyField(Author,
                                          related_name='fiction_book_authors')  # ???
     title = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
 
 
 class ScienceBook(models.Model):
@@ -19,9 +27,12 @@ class ScienceBook(models.Model):
                                          related_name='science_book_authors')  # ???
     title = models.CharField(max_length=300)
     publisher = models.CharField(max_length=200)
-    edition = models.PositiveSmallIntegerField()
+    edition = models.PositiveSmallIntegerField(default=1)
     publishing_year = models.DateField(auto_now=False, auto_now_add=False)
     isbn = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 
 class Article(models.Model):
@@ -29,19 +40,22 @@ class Article(models.Model):
                                          related_name='article_authors')    # ???
     title = models.CharField(max_length=300)
     journal = models.CharField(max_length=300)
-    impact_factor = models.PositiveSmallIntegerField()
-    volume = models.PositiveSmallIntegerField()
-    article_number = models.PositiveSmallIntegerField()
+    impact_factor = models.PositiveSmallIntegerField(default=1)
+    volume = models.PositiveSmallIntegerField(default=1)
+    article_number = models.PositiveSmallIntegerField(default=1)
     pages = models.CharField(max_length=20)    # CharField because of range: "aaa-bbb" pages
     publishing_year = models.DateField(auto_now=False, auto_now_add=False)
     doi = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 
 class LibraryUnit(models.Model):
     fiction_book_unit_type = models.OneToOneField(FictionBook, on_delete=models.CASCADE)
     science_book_unit_type = models.OneToOneField(ScienceBook, on_delete=models.CASCADE)
     article_unit_type = models.OneToOneField(Article, on_delete=models.CASCADE)
-    unit_available = models.BooleanField()
+    unit_available = models.BooleanField(default=True)
 
 
 class UnitStatus(models.Model):
@@ -56,6 +70,9 @@ class UnitStatus(models.Model):
 class LibraryUserInfo(models.Model):
     library_user = models.ForeignKey(User, on_delete=models.CASCADE)
     phone_number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.library_user.__str__()
 
 
 class LibraryUserAddress(models.Model):
@@ -72,3 +89,4 @@ class CitiesList(models.Model):
 class StreetsList(models.Model):
     street_info = models.OneToOneField(LibraryUserAddress, on_delete=models.CASCADE)
     street_name = models.CharField(max_length=200)
+
